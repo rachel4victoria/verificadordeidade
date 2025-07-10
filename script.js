ffab172d73b446d9146243ff8ff10e21976e80da
Rachel Pereira <116238811+rachel4victoria@users.noreply.github.com>
2025-07-10 18:22:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src', 'bebemenino.png')
+
+            }else if (idade < 21){
+                img.setAttribute('src', 'jovemhomem.png')
+
+            }else if (idade < 50){
+                img.setAttribute('src', 'adultohomem.png')
+
+            }else{
+                img.setAttribute('src', 'idosohomem.png')
+
+            }
+
+
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src', 'bebemenina.png')
+
+            }else if (idade < 21){
+                img.setAttribute('src', 'jovemmulher.png')
+
+            }else if (idade < 50){
+                img.setAttribute('src', 'adultomulher.png')
+
+            }else{
+                img.setAttribute('src', 'idosamulher.png')
+                
+            }
+
+
+        }
+        
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    
+    }
+}
